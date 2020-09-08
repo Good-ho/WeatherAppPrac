4d92512f378c0002f2ebe0348771a8ccee2c94a9
Good-ho <whdxo100@gmail.com>
2020-09-08 21:33:51 +0900

    Displaying Temperature / Condition
    	1. Yarn add prop-types
    
    	2. Api를 통해 받은 temp를 표시하는 weather.js acitivity(view)를 하나더 생성하여 온도 값 표시.
    
    Condition을 표시하기 위해, 표시가능한 condition list를 알아야함. 그리고 getter로 얻은 condition을 단순히 뿌려주면됨.

--- App.js
+++ App.js
@@ -1,56 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Alert } from 'react-native';
-import * as Location from 'expo-location';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Alert } from "react-native";
+import * as Location from "expo-location";
 import Loading from "./Loading";
 import axios from "axios";
+import Weather from "./Weather";
 
 const API_KEY = "3284224d113c05984cb203fb2e55fb8f";
 
-export default class extends React.Component{
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
 
-  state={
-    isLoading : true
-  }
-
-  getPermisssion = async() => {
+  getPermisssion = async () => {
     let { status } = await Location.requestPermissionsAsync();
-      if (status !== 'granted') {
-        console.log("permission error");
-      }      
-  }
+    if (status !== "granted") {
+      console.log("permission error");
+    }
+  };
 
-  getLocation = async() => {
-    const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({});
+  getLocation = async () => {
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({});
     console.log(latitude, longitude);
     // send to API and get Weather.
     this.getWeather(latitude, longitude);
-  }
+  };
 
-  getWeather = async(latitude, longitude) => {
+  getWeather = async (latitude, longitude) => {
     const {data} = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
-    )
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      temp : data.main.temp,
+      condition: data.weather[0].main
+    });
+
     console.log(data);
-  }
-  
+  };
+
   componentDidMount() {
     try {
       // throw Error();
       this.getPermisssion();
-
       this.getLocation();
-
-      // this.setState({isLoading:false})
-    } catch(error) {
+    } catch (error) {
       Alert.alert("Can't find you", "so sad");
     }
   }
 
-  render(){
-    const {isLoading} = this.state;
-    return isLoading ? <Loading/> : null ;
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
-
-
